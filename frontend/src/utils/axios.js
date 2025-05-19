5796be4adf649aba2a68315106357c2c07376893
sriharsha <sriharshadabbiru04@gmail.com>
2025-05-19 16:16:55 +0530

    axios update

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://talk-a-tive-backend.onrender.com",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default instance;
