27f3b275c0fde7cfb5dce60a140fd082f8adaa9e
sriharsha <sriharshadabbiru04@gmail.com>
2025-05-21 23:42:43 +0530

    react api url

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 // import Lottie from "react-lottie";
 // import animationData from "../Animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = (process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL.split(",")[0] : "http://localhost:5000");
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL.split(",")[0]
+    : "http://localhost:5000",
 });
 
 export default instance;
